70d6fe7e24d911a550e6e611b5b4ec2f67799799
Mwangimark <132432689+Mwangimark@users.noreply.github.com>
2024-02-29 08:25:02 +0000

    added art logo

--- main.py
+++ main.py
@@ -16,6 +16,9 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+import art
+
+print(art.logo)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 import random 
